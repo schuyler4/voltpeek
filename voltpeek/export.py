27ce2cd0bd7ced00546833ccd8d59d2e1a3b13bc
Marek Newton <marek.s.newton@gmail.com>
2024-08-31 20:36:07 -0500

    fix probe div multiplier on png export

--- voltpeek/export.py
+++ voltpeek/export.py
@@ -11,7 +11,7 @@ def export_png(map: list[list[tuple[int]]], vertical_setting: float, horizontal_
     # TODO: make the font universal
     font = ImageFont.truetype('/usr/share/fonts/truetype/liberation/LiberationMono-Regular.ttf', 20)
     draw = ImageDraw.Draw(image)
-    draw.text((10, 5), str(vertical_setting) + ' V/div', font=font)
+    draw.text((10, 5), str(vertical_setting*probe_div) + ' V/div', font=font)
     draw.text((10, 30), str(horizontal_setting) + ' s/div', font=font)
     draw.text((10, 55), 'Probe: ' + str(probe_div) + 'X', font=font)
     image.save(filename + '.png')
