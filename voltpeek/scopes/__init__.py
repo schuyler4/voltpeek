c242b60220197d2448e6bd23287ec4c5922b219c
Marek Newton <marek.s.newton@gmail.com>
2024-12-17 11:02:47 -0600

    command input tests

--- voltpeek/scopes/__init__.py
+++ voltpeek/scopes/__init__.py
@@ -1,4 +1,3 @@
 from voltpeek.scopes.NS1 import NS1
-from voltpeek.scopes.AD3 import AD3
 
-def get_available_scopes(): return [{NS1.ID:NS1}, {AD3.ID:AD3}]
+def get_available_scopes(): return [{NS1.ID:NS1}]
