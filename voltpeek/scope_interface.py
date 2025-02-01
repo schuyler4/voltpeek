09f8369ca0113c70e0ae4549929258de44e3b820
Marek Newton <marek.s.newton@gmail.com>
2025-02-01 16:18:11 -0600

    scope interface refactor

--- voltpeek/scope_interface.py
+++ voltpeek/scope_interface.py
@@ -1,6 +1,7 @@
-from typing import Optional, Callable
+from typing import Optional, Callable, Dict, Any
 from enum import Enum
 from threading import Thread, Lock
+from functools import wraps
 
 from serial.serialutil import PortNotOpenError
 
@@ -18,6 +19,16 @@ class ScopeAction(Enum):
     SET_FALLING_EDGE_TRIGGER = 10
     SET_AMPLIFIER_GAIN = 11
 
+def scope_action_handler(func):
+    """Decorator for handling scope actions with common cleanup."""
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        result = func(self, *args, **kwargs)
+        self._action_complete = True
+        self._data_available.release()
+        return result
+    return wrapper
+
 class ScopeInterface:
     def __init__(self, scope):
         self._scope_connected: bool = False
@@ -33,121 +44,106 @@ class ScopeInterface:
         self._fs: Optional[float] = None
         self._disconnected_error: bool = False
 
+        # Action handler mapping
+        self._action_handlers: Dict[ScopeAction, Callable] = {
+            ScopeAction.CONNECT: self._connect_scope,
+            ScopeAction.FORCE_TRIGGER: self._force_trigger,
+            ScopeAction.TRIGGER: self._trigger,
+            ScopeAction.SET_CLOCK_DIV: self._set_clock_div,
+            ScopeAction.SET_TRIGGER_LEVEL: self._set_trigger_level,
+            ScopeAction.STOP: self.stop_trigger,
+            ScopeAction.READ_CAL_OFFSETS: self._read_cal_offsets,
+            ScopeAction.SET_CAL_OFFSETS: self._set_cal_offsets,
+            ScopeAction.SET_RANGE: self._set_range,
+            ScopeAction.SET_RISING_EDGE_TRIGGER: self._set_rising_edge_trigger,
+            ScopeAction.SET_FALLING_EDGE_TRIGGER: self._set_falling_edge_trigger,
+            ScopeAction.SET_AMPLIFIER_GAIN: self._set_amplifier_gain,
+        }
+
     def _scope_available(self, scope_action: Callable):
         try:
             scope_action()
-        except Exception or OSError as _:
+        except (Exception, OSError) as _:
             self._disconnected_error = True
 
+    @scope_action_handler
     def _connect_scope(self):
         self._scope.connect()
         self._scope_connected = True
-        self._action_complete = True
-        self._data_available.release()
 
+    @scope_action_handler
     def _force_trigger(self):
-        self._xx: list[int] = self._scope.get_scope_force_trigger_data(self._full_scale)
-        self._action_complete = True
-        self._data_available.release()
+        self._xx = self._scope.get_scope_force_trigger_data(self._full_scale)
 
+    @scope_action_handler
     def _trigger(self):
-        self._xx: list[int] = self._scope.get_scope_trigger_data(self._full_scale)
-        self._action_complete = True
-        self._data_available.release()
+        self._xx = self._scope.get_scope_trigger_data(self._full_scale)
 
+    @scope_action_handler
     def _set_clock_div(self):
         self._scope.set_clock_div(self._value)
-        self._action_complete = True
-        self._data_available.release()
 
+    @scope_action_handler
     def _set_range(self):
         self._scope.set_range(self._full_scale)
-        self._action_complete = True
-        self._data_available.release()
 
+    @scope_action_handler
     def _set_amplifier_gain(self):
         self._scope.set_amplifier_gain(self._full_scale)
-        self._action_complete = True
-        self._data_available.release()
 
+    @scope_action_handler
     def _set_trigger_level(self):
         self._scope.set_trigger_voltage(self._value, self._full_scale)
-        self._action_complete = True
-        self._data_available.release()
 
+    @scope_action_handler
     def _read_cal_offsets(self):
         self._calibration_ints = self._scope.read_calibration_offsets()
-        self._action_complete = True
-        self._data_available.release()
-    
+
+    @scope_action_handler
     def _set_cal_offsets(self):
         self._scope.set_calibration_offsets(self._full_scale)
-        self._action_complete = True
-        self._data_available.release()
 
+    @scope_action_handler
     def _set_rising_edge_trigger(self):
         self._scope.set_rising_edge_trigger()
-        self._action_complete = True
-        self._data_available.release()
 
+    @scope_action_handler
     def _set_falling_edge_trigger(self):
         self._scope.set_falling_edge_trigger()
-        self._action_complete = True
-        self._data_available.release()
 
     def run(self):
-        if self._action == ScopeAction.CONNECT and not self._action_complete:
-            thread: Thread = Thread(target=lambda: self._scope_available(self._connect_scope))  
-        if self._action == ScopeAction.FORCE_TRIGGER and not self._action_complete:
-            thread: Thread = Thread(target=lambda: self._scope_available(self._force_trigger))
-        if self._action == ScopeAction.TRIGGER and not self._action_complete:
-            thread: Thread = Thread(target=lambda: self._scope_available(self._trigger))
-        if self._action == ScopeAction.SET_CLOCK_DIV and not self._action_complete:
-            thread: Thread = Thread(target=lambda: self._scope_available(self._set_clock_div))
-        if self._action == ScopeAction.SET_TRIGGER_LEVEL and not self._action_complete:
-            thread: Thread = Thread(target=lambda: self._scope_available(self._set_trigger_level))
-        if self._action == ScopeAction.STOP and not self._action_complete:
-            thread: Thread = Thread(target=lambda: self._scope_available(self.stop_trigger))
-        if self._action == ScopeAction.READ_CAL_OFFSETS and not self._action_complete:
-            thread: Thread = Thread(target=lambda: self._scope_available(self._read_cal_offsets))
-        if self._action == ScopeAction.SET_CAL_OFFSETS and not self._action_complete:
-            thread: Thread = Thread(target=lambda: self._scope_available(self._set_cal_offsets))
-        if self._action == ScopeAction.SET_RANGE and not self._action_complete:
-            thread: Thread = Thread(target=lambda: self._scope_available(self._set_range))
-        if self._action == ScopeAction.SET_RISING_EDGE_TRIGGER and not self._action_complete:
-            thread: Thread = Thread(target=lambda: self._scope_available(self._set_rising_edge_trigger))
-        if self._action == ScopeAction.SET_FALLING_EDGE_TRIGGER and not self._action_complete:
-            thread: Thread = Thread(target=self._set_falling_edge_trigger)
-        if self._action == ScopeAction.SET_AMPLIFIER_GAIN and not self._action_complete:
-            thread: Thread = Thread(target=self._set_amplifier_gain)
-        thread.start()
-
-    @property 
-    def data_available(self): return not self._data_available.locked()
+        if not self._action_complete and self._action in self._action_handlers:
+            thread = Thread(
+                target=lambda: self._scope_available(self._action_handlers[self._action])
+            )
+            thread.start()
 
     @property
-    def xx(self): return self._xx
+    def data_available(self) -> bool: return not self._data_available.locked()
 
     @property
-    def value(self): return self._value
+    def xx(self) -> Optional[list[float]]: return self._xx
 
     @property
-    def calibration_ints(self): return self._calibration_ints
+    def value(self) -> Optional[int]: return self._value
 
     @property
-    def stopped(self): return self._scope.stopped
+    def calibration_ints(self) -> Optional[list[int]]: return self._calibration_ints
 
     @property
-    def stop_flag(self): return self._stop_flag
+    def stopped(self) -> bool: return self._scope.stopped
+
+    @property
+    def stop_flag(self) -> bool: return self._stop_flag
 
     @property
     def scope(self): return self._scope 
 
     @property
-    def full_scale(self): return self._full_scale
+    def full_scale(self) -> float: return self._full_scale
 
     @property
-    def disconnected_error(self): return self._disconnected_error
+    def disconnected_error(self) -> bool: return self._disconnected_error
 
     def set_value(self, new_value: int) -> None:
         if self.data_available:
@@ -170,7 +166,7 @@ class ScopeInterface:
     def clear_disconnected_error(self): self._disconnected_error = False
 
     @property
-    def fs(self): return self._fs
+    def fs(self) -> Optional[float]: return self._fs
 
     @fs.setter
     def fs(self, new_fs: float): self._fs = new_fs
