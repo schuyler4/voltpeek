78de0a72d17252bd9d10247e8508563a24bff9de
Marek Newton <marek.s.newton@gmail.com>
2025-02-10 18:02:47 -0600

    change screen size

--- voltpeek/constants.py
+++ voltpeek/constants.py
@@ -12,7 +12,7 @@ class Serial_Protocol:
     BUFFER_FLUSH_DELAY:float = 1
 
 class Display:
-    SIZE:int = 800
+    SIZE:int = 500
     BACKGROUND_COLOR:str = 'black'
     GRID_LINE_COUNT:int = 10
     GRID_LINE_COLOR:str = 'grey'
