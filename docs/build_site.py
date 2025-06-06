2c3e2a1c87b5faa9426463407dc06262f06f6cb5
Marek Newton <marek.s.newton@gmail.com>
2025-06-05 23:42:06 -0500

    added command docs

--- docs/build_site.py
+++ docs/build_site.py
@@ -1,7 +1,7 @@
 from voltpeek import commands
 
-with open('../schuyler4.github.io/index.html.template', 'r') as infile:
+with open('./commands.html.template', 'r') as infile:
     template = infile.read()
     template += commands.__doc__
-    with open('../schuyler4.github.io/index.html', 'w') as outfile:
+    with open('./commands.html', 'w') as outfile:
         outfile.write(template)
